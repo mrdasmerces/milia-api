65d6f12fbc47152feacb8bf2d6b9e04146292626
Matheus das Mercês <labsadministrator@Matheus-Bezerra-Macbook-Pro.local>
2019-09-25 13:03:30 -0300

    Build final telegram channel with typing and response

--- src/handlers/channels/telegram.js
+++ src/handlers/channels/telegram.js
@@ -1,20 +1,89 @@
 'use strict';
 
+const axios = require('axios');
+
 const getUserId = async (requestBody) => {
-  return requestBody.message.from.id;
+  return requestBody.message.from.id.toString();
 };
 
 const getParametersRequest = async (requestBody) => {
-  const paramsUser = {};
+  const paramsUser = {
+    chat_id: requestBody.message.chat.id,
+  };
   return { queryText: requestBody.message.text, paramsUser };
 }
 
-const sendTyping = async () => {
+const sendTyping = async (paramsUser) => {
   console.log('channel: Telegram - Milia está digitando...');
+  const payloadTyping = {
+    StatusCode: 200,
+    StatusDescription: 'OK',
+    chat_id: paramsUser.chat_id,
+    action: 'typing',
+  };
+
+  const { post } = axios.create({
+    baseURL: 'https://api.telegram.org',
+    timeout: +process.env.TIMEOUT,
+    headers: {
+      'Content-Type': 'application/json',
+      'cache-Control': 'no-cache',
+    },
+  });
+
+  const response = await post(`/bot${process.env.TELEGRAM_HASH}/sendChatAction`, payloadTyping);
+  return response;
 };
 
-const endMessageRequest = async (dialogflowResult) => {
-  console.log(dialogflowResult);
+const endMessageRequest = async (dialogflowResult, paramsUser) => {
+
+  const { post } = axios.create({
+    baseURL: 'https://api.telegram.org',
+    timeout: +process.env.TIMEOUT,
+    headers: {
+      'Content-Type': 'application/json',
+      'cache-Control': 'no-cache',
+    },
+  });
+
+  const promises = [];
+  for(const message of dialogflowResult) {
+
+    const payload = {
+      StatusCode: 200,
+      StatusDescription: 'OK',
+      chat_id: paramsUser.chat_id,
+      parse_mode: 'Markdown',      
+    };
+
+    if(message.text) {
+      payload.text = message.text;
+    }
+
+    if(message.template) {
+      payload.text = message.template.payload.text;
+
+      payload.reply_markup = {
+        inline_keyboard: [[]]
+      }
+
+      for(const button of message.template.payload.buttons) {
+        payload.reply_markup.inline_keyboard[0].push({
+          text: button.title,
+          url: button.url
+        });
+      }
+
+      payload.reply_markup = JSON.stringify(payload.reply_markup);
+    }
+
+    promises.push(post(`/bot${process.env.TELEGRAM_HASH}/sendMessage`, payload))
+
+  }
+
+  const response = Promise.all(promises);
+
+  return response;
 }
 
 module.exports = {
